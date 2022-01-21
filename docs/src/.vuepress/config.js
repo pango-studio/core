2bd48dcead9bdf27cfc1f5791e70fd690d0d1fb6
Jason Tame <jason@advantagelearn.com>
2022-01-21 17:41:08 +0200

    Remove missing page from vuepress docs

--- docs/src/.vuepress/config.js
+++ docs/src/.vuepress/config.js
@@ -46,7 +46,7 @@ module.exports = {
                 {
                     title: "Guide",
                     collapsable: false,
-                    children: ["", "using-vue"],
+                    children: ["", "permissions-roles"],
                 },
             ],
         },
---
+++ docs/src/guide/permissions-roles.md
@@ -0,0 +1,5 @@
+# Permissions and Roles
+
+## Permissions
+
+## Roles
