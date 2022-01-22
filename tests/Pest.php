f333366c774fb579a7da29aae8adcb130c86de27
Jason Tame <jason@advantagelearn.com>
2022-01-22 11:12:05 +0200

    Put all tests in one folder, remove unit and feature folders

--- tests/Pest.php
+++ tests/Pest.php
@@ -3,4 +3,5 @@
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Salt\Core\Tests\TestCase;
 
-uses(TestCase::class, RefreshDatabase::class)->in('Feature', 'Unit');
+
+uses(TestCase::class, RefreshDatabase::class)->in('');
