78cd9c322ad84e3a528c82fa042026a885df2ee0
Jason Tame <jason@advantagelearn.com>
2022-01-26 11:07:10 +0200

    Add auth0 callback to list of routes added during package installation

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
+use Auth0\Login\Auth0Controller;
 use Illuminate\Support\Facades\Route;
-
 use Salt\Core\Http\Controllers\Auth\Auth0IndexController;
 
+Route::get('/auth0/callback', [Auth0Controller::class, 'callback'])->name('auth0-callback');
 Route::get('/login', [Auth0IndexController::class, 'login'])->name('login');
 Route::get('/logout', [Auth0IndexController::class, 'logout'])->name('logout');
