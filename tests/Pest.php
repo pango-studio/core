9eeb7b31d80c242e80662fda8f52216b1b328382
Jason Tame <jason@advantagelearn.com>
2022-02-04 11:16:47 +0200

    Add actingAs test helper

--- tests/Pest.php
+++ tests/Pest.php
@@ -1,6 +1,15 @@
 <?php
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use Salt\Core\Tests\TestCase;
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 uses(TestCase::class, RefreshDatabase::class)->in('');
+
+/**
+ * Set the currently logged in user for the application.
+ */
+function actingAs(Authenticatable $user, string $driver = null): TestCase
+{
+    return test()->actingAs($user, $driver);
+}
