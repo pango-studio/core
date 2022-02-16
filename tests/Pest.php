beabff21038533820eadc44f0b1aaf0ce1193123
Jason Tame <jason@advantagelearn.com>
2022-02-16 10:05:59 +0200

    Remove illuminate/contracts dependency as its blocking laravel 9 upgrade

--- tests/Pest.php
+++ tests/Pest.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Salt\Core\Tests\TestCase;
 
@@ -9,7 +8,7 @@ uses(TestCase::class, RefreshDatabase::class)->in('');
 /**
  * Set the currently logged in user for the application.
  */
-function actingAs(Authenticatable $user, string $driver = null): TestCase
+function actingAs($user, string $driver = null): TestCase
 {
     return test()->actingAs($user, $driver);
 }
