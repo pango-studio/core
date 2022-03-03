065369476aa14bb34db62ba8e15f06956b27f80d
Jason Tame <jason@advantagelearn.com>
2022-03-03 11:36:04 +0200

    Fix bug where perPage GET variable remains after test runs

--- tests/PerPagePaginateTraitTest.php
+++ tests/PerPagePaginateTraitTest.php
@@ -4,18 +4,15 @@ use function PHPUnit\Framework\assertEquals;
 use Salt\Core\Models\User;
 
 beforeEach(function () {
-    User::factory(20)->create();
+    User::factory(30)->create();
 });
 
 it('paginates with 10 items per page by default', function () {
-
+    $_GET['perPage'] = null; // Ensure that it doesn't remain set after other test runs
     assertEquals(User::perPagePaginate(), User::paginate(10));
 });
 
 it('changes the number of items per page according to the perPage GET variable', function () {
     $_GET['perPage'] = 20;
     assertEquals(User::perPagePaginate(), User::paginate(20));
-
-    $_GET['perPage'] = 30;
-    assertEquals(User::perPagePaginate(), User::paginate(30));
 });
