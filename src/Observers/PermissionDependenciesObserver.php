7ac471365922afe979e5bbbe93ac36b26c18aa1a
Luke Siedle <ljsiedle@gmail.com>
2022-08-12 18:44:47 +0100

    Adds new requirements for managing nested permissions

---
+++ src/Observers/PermissionDependenciesObserver.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Salt\Core\Observers;
+
+use Salt\Core\Models\User;
+use Salt\Core\Models\UserPermission;
+use Salt\Core\Services\UserPermissionService;
+
+class PermissionDependenciesObserver
+{
+
+    public function __construct(UserPermissionService $userPermissionService ){
+        $this->userPermissionService = $userPermissionService;
+    }
+
+    /**
+     * Handle the UserPermission "creating" event.
+     * Throws an error if the user requires more
+     * permissions before adding this one.
+     *
+     * @param  UserPermission  $user_permission
+     * @return void
+     */
+    public function creating(UserPermission $user_permission)
+    {
+        $this->userPermissionService->verifyDependencies($user_permission);
+    }
+}
