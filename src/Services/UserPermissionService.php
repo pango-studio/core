6c59aa64917c1593278d0a47ea62b5b0d9ceefb5
lukesiedle <lukesiedle@users.noreply.github.com>
2022-08-15 07:44:26 +0000

    Fix styling

--- src/Services/UserPermissionService.php
+++ src/Services/UserPermissionService.php
@@ -14,9 +14,9 @@ class UserPermissionService
         $permission = Permission::find($user_permission->permission_id);
         $user = User::find($user_permission->user_id);
         $dependencies = $permission->dependencies;
-        foreach( $dependencies as $perm ){
-            if( !$user->hasPermission($perm) ){
-                throw new PermissionDependencyException('User requires ' . $perm->name . ' permission in order to be granted ' . $permission->name );
+        foreach ($dependencies as $perm) {
+            if (!$user->hasPermission($perm)) {
+                throw new PermissionDependencyException('User requires ' . $perm->name . ' permission in order to be granted ' . $permission->name);
             }
         }
     }
